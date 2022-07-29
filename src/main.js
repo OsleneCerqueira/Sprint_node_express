42101ce53a01ec9c314514384267eaebe42a1798
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-07-28 22:31:23 -0300

    Implementação da ferramenta

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+
+
+function ajudaDev() {
+
+    let listaPropriedades = []
+
+    let propriedade = readlineSync.question('Digite a propriedades de CSS que deseja armazenar: ')
+
+    while (propriedade !== "sair") {
+        listaPropriedades.push(propriedade);
+
+        propriedade = readlineSync.question('Digite a propriedades de CSS que deseja armazenar: ')
+    }
+
+
+    console.log(listaPropriedades.sort().join('\n'))
+}
+
+ajudaDev()
+
+
+
+
+
+
